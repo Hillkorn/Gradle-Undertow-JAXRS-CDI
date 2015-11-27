5412e4e678ea166b02bbaa628056788ab8633763
Dennis Schumann <hillkorn@gmail.com>
2015-11-27 14:23:41 +0100

    Deltaspike cdi added.

--- src/main/java/de/hillkorn/MyApplication.java
+++ src/main/java/de/hillkorn/MyApplication.java
@@ -9,6 +9,11 @@ import javax.servlet.ServletException;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Application;
+import org.apache.deltaspike.cdise.api.CdiContainer;
+import org.apache.deltaspike.cdise.api.CdiContainerLoader;
+import org.apache.deltaspike.cdise.api.ContextControl;
+import org.apache.deltaspike.cdise.servlet.CdiServletRequestListener;
+import org.jboss.resteasy.plugins.server.servlet.ResteasyBootstrap;
 import org.jboss.resteasy.plugins.server.undertow.UndertowJaxrsServer;
 import org.jboss.resteasy.spi.ResteasyDeployment;
 import org.reflections.Reflections;
@@ -17,58 +22,57 @@ import org.reflections.Reflections;
 @ApplicationScoped
 public class MyApplication extends Application {
 
-    private static final int PORT = 8080;
+  private static final int PORT = 8080;
 
-    public static void main(String[] args) throws ServletException {
-        //HTTP Server
-        UndertowJaxrsServer server = new UndertowJaxrsServer();
-        Undertow.Builder serverBuilder = Undertow.builder().addHttpListener(PORT, "0.0.0.0");
-        server.start(serverBuilder);
-//        CdiContainer cdiContainer = CdiContainerLoader.getCdiContainer();
-//        cdiContainer.boot();
-//
-//        ContextControl contextControl = cdiContainer.getContextControl();
-//        contextControl.startContexts();
+  public static void main(String[] args) throws ServletException {
+    //HTTP Server
+    UndertowJaxrsServer server = new UndertowJaxrsServer();
+    Undertow.Builder serverBuilder = Undertow.builder().addHttpListener(PORT, "0.0.0.0");
+    server.start(serverBuilder);
+    CdiContainer cdiContainer = CdiContainerLoader.getCdiContainer();
+    cdiContainer.boot();
+
+    ContextControl contextControl = cdiContainer.getContextControl();
+    contextControl.startContexts();
 //        contextControl.startContext(ApplicationScoped.class);
 
-        ResteasyDeployment deployment = new ResteasyDeployment();
+    ResteasyDeployment deployment = new ResteasyDeployment();
 //        deployment.setInjectorFactoryClass(CdiInjectorFactory.class.getName());
-      deployment.setInjectorFactoryClass("org.jboss.resteasy.cdi.CdiInjectorFactory");
-        deployment.setApplicationClass(MyApplication.class.getName());
+    deployment.setInjectorFactoryClass("org.jboss.resteasy.cdi.CdiInjectorFactory");
+    deployment.setApplicationClass(MyApplication.class.getName());
 
-      DeploymentInfo undertowDeployment = server.undertowDeployment(deployment, "/");
+    DeploymentInfo undertowDeployment = server.undertowDeployment(deployment, "/");
 
-        undertowDeployment.setClassLoader(MyApplication.class.getClassLoader())
+    undertowDeployment.setClassLoader(MyApplication.class.getClassLoader())
             .setContextPath("/")
-              .setDeploymentName("Tryout")
-              .addListeners(Servlets.listener(org.jboss.weld.environment.servlet.Listener.class));
-//              .addListener(Servlets.listener(Listener.class));
- //            .addListener(Servlets.listener(ResteasyBootstrap.class));
-        server.deploy(undertowDeployment);
+            .setDeploymentName("Tryout")
+            .addListener(Servlets.listener(CdiServletRequestListener.class))
+            .addListener(Servlets.listener(ResteasyBootstrap.class));
+    server.deploy(undertowDeployment);
   }
 
-    private final Set<Class<?>> classes;
+  private final Set<Class<?>> classes;
 
-    public MyApplication() {
-        System.out.println("Scan for Paths");
-        Reflections reflections = new Reflections("de.hillkorn.rest");
+  public MyApplication() {
+    System.out.println("Scan for Paths");
+    Reflections reflections = new Reflections("de.hillkorn.rest");
     classes = reflections.getTypesAnnotatedWith(Path.class);
 //      classes.add(TestService.class);
   }
 
-    @Override
-    public Set<Class<?>> getClasses() {
+  @Override
+  public Set<Class<?>> getClasses() {
 //        classes.add(TestService.class);
-        return classes;
+    return classes;
 //        return null;
-    }
+  }
 
-    @Override
-    public Set<Object> getSingletons() {
+  @Override
+  public Set<Object> getSingletons() {
 //      Set<Object> resources = new LinkedHashSet<Object>();
 //      return resources;
-      return null;
-    }
+    return null;
+  }
 
 //    ServletInfo servletInfo = Servlets.servlet("YourServletName", YourServlet.class).setAsyncSupported(true)
 //    .setLoadOnStartup(1).addMapping("/*");
