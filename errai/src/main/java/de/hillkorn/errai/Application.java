032d6ff355460e28b8dc8298367426873816fca2
Dennis Schumann <hillkorn@gmail.com>
2016-01-02 16:02:01 +0100

    Working errai.

--- errai/src/main/java/de/hillkorn/errai/Application.java
+++ errai/src/main/java/de/hillkorn/errai/Application.java
@@ -1,13 +1,15 @@
 package de.hillkorn.errai;
 
 import javax.annotation.PostConstruct;
+import org.jboss.errai.enterprise.client.jaxrs.api.RestClient;
 import org.jboss.errai.ioc.client.api.EntryPoint;
 
 @EntryPoint
 public class Application {
-    
+
     @PostConstruct
     public void init() {
-//        Window.alert("Hello World");
+        RestClient.setJacksonMarshallingActive(true);
+        RestClient.setApplicationRoot("/api");
     }
 }
