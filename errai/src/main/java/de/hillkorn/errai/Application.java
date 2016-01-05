584595f5a2331ea01bde46985eee65a687536664
Dennis Schumann <hillkorn@gmail.com>
2016-01-05 10:23:49 +0100

    Added navigation to errai EntryPoint.

--- errai/src/main/java/de/hillkorn/errai/Application.java
+++ errai/src/main/java/de/hillkorn/errai/Application.java
@@ -1,11 +1,16 @@
 package de.hillkorn.errai;
 
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 import org.jboss.errai.enterprise.client.jaxrs.api.RestClient;
 import org.jboss.errai.ioc.client.api.EntryPoint;
+import org.jboss.errai.ui.nav.client.local.Navigation;
 
 @EntryPoint
 public class Application {
+
+    @Inject
+    Navigation navigation;
 
     @PostConstruct
     public void init() {
